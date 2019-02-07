df2dcad241c60683305c3210613261a137592e80
Anders Hansen <xiplias@gmail.com>
2019-02-07 14:31:17 +0100

    Fix work config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,5 +2,6 @@ import CMS from "netlify-cms";
 
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import WorkPreview from "./preview-templates/WorkPreview";
+
 CMS.registerPreviewTemplate("work", WorkPreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
