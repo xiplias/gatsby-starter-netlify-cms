45d5bd5e6bc391a923c1faa8191e09ce9462b5a6
Anders Hansen <xiplias@gmail.com>
2019-02-07 14:26:06 +0100

    Remove about and revert package.json

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,6 @@
 import CMS from "netlify-cms";
 
-import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import WorkPreview from "./preview-templates/WorkPreview";
-
-CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("work", WorkPreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
